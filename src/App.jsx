3ffed75b8a64145cdfcf6a5468dd2a4babf51898
Lamia Fatiha <f6lamia@gmail.com>
2024-06-22 12:38:19 +0600

    navbar  done

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,25 @@
 import React from 'react'
-import 
+import '../node_modules/bootstrap/dist/css/bootstrap.min.css';
+import '../node_modules/bootstrap/dist/js/bootstrap.bundle';
 
+import Home from './Home';
+import About from './About';
+import Contact from './Contact';
+import Services from './Services';
+import { Routes, Route, Navigate } from 'react-router-dom';
+import Navbar from './Navbar';
 const App = () => {
     return (
         <>
-           <h1>Welcome</h1>
-        <
+        <Navbar />
+        <Routes>
+            <Route path='/' Component={Home}></Route>
+            <Route path='/about' Component={About}></Route>
+            <Route path='/service' Component={Services}></Route>
+            <Route path='/contact' Component={Contact}></Route>
+            <Route path="*" element={<Navigate to="/" />} /></Routes>
+        </>
+    )
+};
+
+export default App;
